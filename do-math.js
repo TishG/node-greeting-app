ef61839c6d50d04ac2dd59390565203f48708dfb
Tish <latisha.griffiths@gmail.com>
2018-10-30 17:24:27 -0500

    checkpoint: Basic Node App - completed

---
+++ do-math.js
@@ -0,0 +1,7 @@
+exports.sum = function(a,b) {
+  console.log(a + b);
+}
+
+exports.subtract = function(a,b) {
+  console.log(a - b);
+}
---
+++ main.js
@@ -0,0 +1,13 @@
+const greetings = require("./greeting.js");
+
+const math = require("./do-math.js");
+
+greetings.greetingEnglish();
+
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic();
+
+math.sum(1,2);
